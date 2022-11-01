a0d8bbf3e35ed1b7ad1321dc74ec96cf8545bad1
gavin-ko <gavin.meattiger@gmail.com>
2022-11-01 23:39:02 +0900

    feat : 중복 지점 찾는 기능
    문제 4번 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,47 @@
 package onboarding;
 
 public class Problem2 {
+    public static int findStart(String cryptogram) {
+        int start = -1;
+        for(int i = 0; i<cryptogram.length()-1; i++) {
+            if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                start = i; break;
+            }
+        }
+        return start;
+    }
+    public static int findEnd(String cryptogram, int start) {
+        int end = start;
+        for(int i = start; i<cryptogram.length(); i++) {
+            if(cryptogram.length() == 2) {
+                return start + 1;
+            }
+            if(cryptogram.charAt(start) != cryptogram.charAt(i+1)) {
+                end = i;
+                break;
+            }
+        }
+        return end;
+    }
+
+    public static String decodeWord(String cryptogram, int start, int end) {
+
+    }
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while(true) {
+
+            int start = findStart(cryptogram);
+
+            if(start != -1) {
+                int end = findEnd(cryptogram, start);
+
+                String word = decodeWord(cryptogram, start, end);
+                if(word.equals(cryptogram)) {
+                    break;
+                }
+                cryptogram = word;
+            } else break;
+        }
+        return cryptogram;
     }
 }
