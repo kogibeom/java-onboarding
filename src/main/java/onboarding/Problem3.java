66ca3257f99e88da75861cf442ed9b54bdf6a3cf
gavin-ko <gavin.meattiger@gmail.com>
2022-11-01 23:33:01 +0900

    feat: 입력 값 까지 3,6,9 개수를 세어주는 기능 구현
    문제 3번 기능 구현 재등록, pash 오류로 인한 commit 수정

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
 public class Problem3 {
+    private static int getCount(int num) {
+        int count = 0;
+        while (num != 0) {
+            if(num%10 != 0 && num%10%3 ==0) {
+                count++;
+            }
+            num /= 10;
+        }
+        return count;
+    }
+
     public static int solution(int number) {
         int answer = 0;
+        for(int i =1; i<=number; i++) {
+            answer += getCount(i);
+        }
+
         return answer;
     }
 }
